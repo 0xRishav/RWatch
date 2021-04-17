104048663a225bba4657b82ac4fb94aa07a57b04
Rishav Bharti <rshvbharti@gmail.com>
2021-04-17 15:34:52 +0530

    Added some video data

---
+++ src/data/AllVideos.js
@@ -0,0 +1,108 @@
+export const AllVideos = [
+  // APPLE
+  {
+    title: "WWDC 2020",
+    organizer: "Apple",
+    description:
+      "Apple WWDC 2020 kicked off with big announcements, exciting reveals, inspiration, and new opportunities to create the most innovative apps in the world.",
+    isLatest: true,
+    videoId: "GEZhD3J89ZE",
+    videoUrl: "https://www.youtube.com/watch?v=GEZhD3J89ZE",
+  },
+
+  {
+    title: "High Speed - October 13",
+    organizer: "Apple",
+    description:
+      "Watch the special Apple Event and learn about the latest updates for HomePod mini, iPhone, and more.",
+    isLatest: false,
+    videoId: "KR0g-1hnQPA",
+    videoUrl: "https://www.youtube.com/watch?v=KR0g-1hnQPA",
+  },
+
+  //   GOOGLE
+
+  {
+    title: "Google Keynote (Google I/O'19)",
+    organizer: "Google",
+    description:
+      "Learn about the latest product and platform innovations at Google in a Keynote led by Sundar Pichai.",
+    isLatest: true,
+    videoId: "lyRPyRKHO8M",
+    videoUrl: "https://www.youtube.com/watch?v=lyRPyRKHO8M",
+  },
+
+  {
+    title: "Google Keynote (Google I/O'18)",
+    organizer: "Google",
+    description:
+      "Learn about the latest product and platform innovations at Google in a Keynote led by Sundar Pichai.",
+    isLatest: false,
+    videoId: "ogfYd705cRs",
+    videoUrl: "https://www.youtube.com/watch?v=ogfYd705cRs",
+  },
+
+  //   SAMSUNG
+
+  {
+    title: "Galaxy Unpacked January 2021",
+    organizer: "Samsung",
+    description:
+      "It’s the moment we’ve all been waiting for. #SamsungUnpacked​ was LIVE on January 14, 2021.",
+    isLatest: true,
+    videoId: "TD_BZN0bn_U",
+    videoUrl: "https://www.youtube.com/watch?v=TD_BZN0bn_U",
+  },
+
+  {
+    title: "Samsung Galaxy Awesome Unpacked March 2021",
+    organizer: "Samsung",
+    description:
+      "Ready for the next generation of awesome? #SamsungUnpacked​ #GalaxyA72​ #GalaxyA52​ #Awesome",
+    isLatest: true,
+    videoId: "17Tc8un6J40",
+    videoUrl: "https://www.youtube.com/watch?v=17Tc8un6J40",
+  },
+
+  //MICROSOFT
+
+  {
+    title: "Microsoft Ignite 2021",
+    organizer: "Microsoft",
+    description:
+      "Join us for our digital event experience on March 2-4, 2021. Microsoft Ignite is for IT pros, decision makers, implementors, architects, developers, and data professionals to explore the latest tools, receive deep technical training, and get questions answered by Microsoft experts.",
+    isLatest: true,
+    videoId: "Bha8qs4wB_8",
+    videoUrl: "https://www.youtube.com/watch?v=Bha8qs4wB_8",
+  },
+
+  {
+    title: "#MicrosoftEvent​ Live",
+    organizer: "Microsoft",
+    description:
+      "See what’s next from Microsoft Surface at the #MicrosoftEvent​ in New York City.",
+    isLatest: false,
+    videoId: "dmaioTs0NH8",
+    videoUrl: "https://www.youtube.com/watch?v=dmaioTs0NH8",
+  },
+
+  //   CES
+
+  {
+    title: "The Best of CES 2020",
+    organizer: "CES",
+    description: "See The Best of CES 2020.",
+    isLatest: true,
+    videoId: "YvRP_Y1WDxw",
+    videoUrl: "https://www.youtube.com/watch?v=YvRP_Y1WDxw",
+  },
+
+  {
+    title: "The Best of CES 2019",
+    organizer: "Microsoft",
+    description: "The Best of CES 2019: Only the cream of the crop",
+    isLatest: false,
+    videoId: "mHreov2zl1U",
+    videoUrl: "https://www.youtube.com/watch?v=mHreov2zl1U",
+  },
+];
