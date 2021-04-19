c818e7c2a690c89add06845b69f6fef9d7300fbd
Rishav Bharti <rshvbharti@gmail.com>
2021-04-19 13:54:45 +0530

    Created latest row container

--- src/containers/latest-rows/LatestRow.js
+++ src/containers/latest-rows/LatestRow.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LatestVideoCard } from "../../components";
+import { videoContext } from "../../context/VideoContext";
 import "./LatestRow.css";
 
 function LatestRow() {
-  return <div></div>;
+  const AllVideos = useContext(videoContext);
+  const latestVideos = AllVideos.filter((video) => video.isLatest);
+  return (
+    <div className="latestRow">
+      <h1>Latest on RWatch</h1>
+      <div className="latestRow__videosWrapper">
+        {latestVideos.map((video) => (
+          <LatestVideoCard {...video} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default LatestRow;
