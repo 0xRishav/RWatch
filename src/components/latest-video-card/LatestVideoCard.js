6889be7665a2609dd2734b0a7ff3b65fb5308b2e
Rishav Bharti <rshvbharti@gmail.com>
2021-04-19 13:50:37 +0530

    Changed static content to dynamic

--- src/components/latest-video-card/LatestVideoCard.js
+++ src/components/latest-video-card/LatestVideoCard.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { videoContext } from "../../context/VideoContext";
 import "./LatestVideoCard.css";
 
-function LatestVideoCard() {
+function LatestVideoCard({ title, videoId, description, organiser }) {
   const AllVideos = useContext(videoContext);
   // console.log(AllVideos);
 
@@ -12,17 +12,15 @@ function LatestVideoCard() {
   return (
     <div className="latestVideoCard">
       <img
-        src={`https://img.youtube.com/vi/${AllVideos[0].videoId}/0.jpg`}
+        src={`https://img.youtube.com/vi/${videoId}/0.jpg`}
         alt="thumbnail"
         className="latestVideoCard__coverPhoto"
       />
       <div className="latestVideoCard__infoWrapper">
-        <div className="latestVideoCard__organiser">
-          {AllVideos[0].organiser}
-        </div>
-        <div className="latestVideoCard__title">{AllVideos[0].title}</div>
+        <div className="latestVideoCard__organiser">{organiser}</div>
+        <div className="latestVideoCard__title">{truncate(title, 15)}</div>
         <div className="latestVideoCard__description">
-          {truncate(AllVideos[0].description, 80)}
+          {truncate(description, 80)}
         </div>
       </div>
     </div>
