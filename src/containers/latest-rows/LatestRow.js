7224c968f58de460ca148c4493bc0c4bf7c9d560
Rishav Bharti <rshvbharti@gmail.com>
2021-04-20 15:14:28 +0530

    Refactor: Added scroll container for drag scroll

--- src/containers/latest-rows/LatestRow.js
+++ src/containers/latest-rows/LatestRow.js
@@ -1,19 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { LatestVideoCard } from "../../components";
 import { videoContext } from "../../context/VideoContext";
 import "./LatestRow.css";
+import { RiArrowDropRightLine } from "react-icons/ri";
+import ScrollContainer from "react-indiana-drag-scroll";
 
 function LatestRow() {
   const AllVideos = useContext(videoContext);
   const latestVideos = AllVideos.filter((video) => video.isLatest);
+  
+
   return (
     <div className="latestRow">
       <h1>Latest on RWatch</h1>
-      <div className="latestRow__videosWrapper">
+      <ScrollContainer className="latestRow__videosWrapper">
         {latestVideos.map((video) => (
           <LatestVideoCard {...video} />
         ))}
-      </div>
+      </ScrollContainer>
     </div>
   );
 }
