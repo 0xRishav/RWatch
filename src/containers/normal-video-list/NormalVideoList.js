81c702eeb679de37939e23e5f9d15250ced6ef73
Rishav Bharti <rshvbharti@gmail.com>
2021-04-21 17:35:14 +0530

    Added title for more videos

--- src/containers/normal-video-list/NormalVideoList.js
+++ src/containers/normal-video-list/NormalVideoList.js
@@ -3,10 +3,10 @@ import { NormalVideoCard } from "../../components";
 import "./NormalVideoList.css";
 import ScrollContainer from "react-indiana-drag-scroll";
 
-function NormalVideoList({ videos }) {
+function NormalVideoList({ videos, title }) {
   return (
     <div className="normalVideoList">
-      <h1>From {videos[0].organiser}</h1>
+      {title ? <h1>More From {title}</h1> : <h1>From {videos[0].organiser}</h1>}
       <ScrollContainer className="normalVideoList__videosWrapper">
         {videos.map((video) => (
           <NormalVideoCard {...video} />
