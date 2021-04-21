2d4760064ac357c27f57b8e624b6762489d26d3e
Rishav Bharti <rshvbharti@gmail.com>
2021-04-21 17:34:43 +0530

    Added link

--- src/components/normal-video-card/NormalVideoCard.js
+++ src/components/normal-video-card/NormalVideoCard.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { videoContext } from "../../context/VideoContext";
 import "./NormalVideoCard.css";
+import { Link } from "react-router-dom";
 import { AiFillPlayCircle } from "react-icons/ai";
 
 function NormalVideoCard({ videoId, organiser, title }) {
@@ -12,29 +13,31 @@ function NormalVideoCard({ videoId, organiser, title }) {
       onMouseEnter={() => setIsCardHover(true)}
       onMouseLeave={() => setIsCardHover(false)}
     >
-      <img
-        src={`https://img.youtube.com/vi/${videoId}/0.jpg`}
-        alt="video-cover"
-        className="normalVideoCard__cover"
-        style={{ opacity: isCardHover ? "0.5" : "1" }}
-      />
-      <div
-        className="normalVideoCard__organiser"
-        style={{ opacity: isCardHover ? "1" : "0" }}
-      >
-        {organiser}
-      </div>
-      <AiFillPlayCircle
-        className="normalVideoCard__playBtn"
-        size="50"
-        style={{ opacity: isCardHover ? "1" : "0" }}
-      />
-      <div
-        className="normalVideoCard__title"
-        style={{ opacity: isCardHover ? "1" : "0" }}
-      >
-        {title}
-      </div>
+      <Link to={`/videos/${videoId}`} className="normalVideoCard__link">
+        <img
+          src={`https://img.youtube.com/vi/${videoId}/0.jpg`}
+          alt="video-cover"
+          className="normalVideoCard__cover"
+          style={{ opacity: isCardHover ? "0.5" : "1" }}
+        />
+        <div
+          className="normalVideoCard__organiser"
+          style={{ opacity: isCardHover ? "1" : "0" }}
+        >
+          {organiser}
+        </div>
+        <AiFillPlayCircle
+          className="normalVideoCard__playBtn"
+          size="50"
+          style={{ opacity: isCardHover ? "1" : "0" }}
+        />
+        <div
+          className="normalVideoCard__title"
+          style={{ opacity: isCardHover ? "1" : "0" }}
+        >
+          {title}
+        </div>
+      </Link>
     </div>
   );
 }
