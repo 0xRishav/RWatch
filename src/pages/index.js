014122a71d52b9b847290d660b5bf900a69630c9
Rishav Bharti <rshvbharti@gmail.com>
2021-04-23 00:49:35 +0530

    Added sign-in-page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,5 @@ export { default as LandingPage } from "./landing-page/LandingPage";
 export { default as Homepage } from "./homepage/Homepage";
 export { default as VideoPage } from "./videopage/VideoPage";
 export { default as AllVideospage } from "./allvideos/AllVideospage";
+export { default as SignInPage } from "./sign-in-page/SignInPage";
+export { default as SignUpPage } from "./sign-up-page/SignUpPage";
---
+++ src/pages/sign-in-page/SignInPage.js
@@ -0,0 +1,52 @@
+import React, { useContext, useState } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import "./SignInPage.css";
+import { BsArrowUpRight } from "react-icons/bs";
+import { authContext } from "../../context/authContext";
+
+function SignInPage() {
+  //   const { loginWithCredentials } = useContext(authContext);
+  const { loginWithCredentials } = useContext(authContext);
+  const { state } = useLocation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useHistory();
+
+  async function signinHandler() {
+    const response = await loginWithCredentials(email, password);
+    if (response.success) {
+      history.push(state?.from ? state.from : "/");
+    }
+  }
+
+  console.log({ state });
+
+  return (
+    <div className="signin">
+      <h2>Sign in to RStore</h2>
+      <div className="signin__inputContainer">
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="blue-btn--primary" onClick={signinHandler}>
+        Sign in
+      </button>
+      <div>
+        Do not have an RStore account?{" "}
+        <Link to="/signup">
+          Create yours now <BsArrowUpRight />{" "}
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignInPage;
