f3491a37edeac3dcecf588bf7ed8e0abcec9fb32
Rishav Bharti <rshvbharti@gmail.com>
2021-04-23 00:40:37 +0530

    added signin and signout btn

---
+++ src/components/signin-signout-btn/SignInSignOutBtn.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { authContext } from "../../context/authContext";
+import "./SignInSignOutBtn.css";
+
+function SignInSignOutBtn() {
+  const { isUserLoggedIn, setIsUserLoggedIn } = useContext(authContext);
+  const signinBtnHandler = () => {
+    if (isUserLoggedIn) {
+      window.localStorage.removeItem("currentUser");
+      setIsUserLoggedIn(!isUserLoggedIn);
+    }
+  };
+
+  return (
+    <Link to={isUserLoggedIn ? "#" : "/signin"}>
+      <button className="blue-btn--primary" onClick={signinBtnHandler}>
+        {isUserLoggedIn ? "Sign Out" : "Sign In"}
+      </button>
+    </Link>
+  );
+}
+
+export default SignInSignOutBtn;
