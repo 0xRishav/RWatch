f2f040adb3f0b648dfa8ede0ced78950038b1b94
Rishav Bharti <rshvbharti@gmail.com>
2021-04-22 13:14:42 +0530

    exported all videos page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as LandingPage } from "./landing-page/LandingPage";
 export { default as Homepage } from "./homepage/Homepage";
 export { default as VideoPage } from "./videopage/VideoPage";
+export { default as AllVideospage } from "./allvideos/AllVideospage";
