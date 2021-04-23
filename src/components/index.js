6d782c95241af77f2f1f3685de821c3f3131cb05
Rishav Bharti <rshvbharti@gmail.com>
2021-04-23 16:46:46 +0530

    Added Loader

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as LatestVideoCard } from "./latest-video-card/LatestVideoCard"
 export { default as NormalVideoCard } from "./normal-video-card/NormalVideoCard";
 export { default as SignedInNav } from "./signed-in-nav/SignedInNav";
 export { default as SignInSignOutBtn } from "./signin-signout-btn/SignInSignOutBtn";
+export { default as Loader } from "./loader/Loader";
