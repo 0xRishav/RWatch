93e440efd4bd27c929e574848d46bd65c855d4e6
Rishav Bharti <rshvbharti@gmail.com>
2021-04-23 23:16:43 +0530

    Refactor: removed useLocation

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-import { useLocation } from "react-router";
 import useLocalStorage from "../custom-hooks/useLocalStorage";
 import { fakeAuthAPI } from "../helpers";
 
