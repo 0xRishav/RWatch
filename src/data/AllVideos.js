5927ad24a92811bc83a139589219c5decb483b88
Rishav Bharti <rshvbharti@gmail.com>
2021-04-23 13:46:46 +0530

    Added more video data

--- src/data/AllVideos.js
+++ src/data/AllVideos.js
@@ -19,6 +19,33 @@ export const AllVideos = [
     videoId: "KR0g-1hnQPA",
     videoUrl: "https://www.youtube.com/watch?v=KR0g-1hnQPA",
   },
+  {
+    title: "September Event 2019 — Apple",
+    organiser: "Apple",
+    description:
+      "Innovation takes the stage September 10, 2019. Check out the all-new iPhone 11 Pro, iPhone 11, Apple Watch Series 5, and iPad. And get the latest on Apple Arcade and Apple TV+.",
+    isLatest: false,
+    videoId: "-rAeqN-Q7x4",
+    videoUrl: "https://www.youtube.com/watch?v=-rAeqN-Q7x4",
+  },
+  {
+    title: "September Event 2018 — Apple",
+    organiser: "Apple",
+    description:
+      "Apple Special Event. September 12, 2018. Big news all around. Take a look at the all-new Apple Watch Series 4, iPhone XS and iPhone XS Max, and iPhone XR.",
+    isLatest: false,
+    videoId: "wFTmQ27S7OQ",
+    videoUrl: "https://www.youtube.com/watch?v=wFTmQ27S7OQ",
+  },
+  {
+    title: "Apple — WWDC 2017 Keynote",
+    organiser: "Apple",
+    description:
+      "Apple WWDC 2017. Take a look at our latest announcements. The new iPad Pro. iMac Pro. HomePod. iOS 11. macOS High Sierra. watchOS 4.",
+    isLatest: false,
+    videoId: "oaqHdULqet0",
+    videoUrl: "https://www.youtube.com/watch?v=oaqHdULqet0",
+  },
 
   //   GOOGLE
 
@@ -42,6 +69,26 @@ export const AllVideos = [
     videoUrl: "https://www.youtube.com/watch?v=ogfYd705cRs",
   },
 
+  {
+    title: "Google I/O 2017 - Keynote",
+    organiser: "Google",
+    description:
+      "There's something new to love on October 4th. All features, functionality and other product specifications are subject to change without notice or obligation.",
+    isLatest: false,
+    videoId: "0YGoxbeSzLc",
+    videoUrl: "https://www.youtube.com/watch?v=0YGoxbeSzLc",
+  },
+
+  {
+    title: "Google I/O 2016 - Keynote",
+    organiser: "Google",
+    description:
+      "Watch the entire Google I/O Keynote here. Some highlights include the new messaging app Allo, the Video calling app Duo, the latest in Developer Platform innovation with Firebase, What's new in Android N, Android Wear 2.0 updates, and Android Instant Run.",
+    isLatest: false,
+    videoId: "862r3XS2YB0",
+    videoUrl: "https://www.youtube.com/watch?v=862r3XS2YB0",
+  },
+
   //   SAMSUNG
 
   {
@@ -63,6 +110,33 @@ export const AllVideos = [
     videoId: "17Tc8un6J40",
     videoUrl: "https://www.youtube.com/watch?v=17Tc8un6J40",
   },
+  {
+    title: "Galaxy Note20: 2020 Unpacked Official Replay | Samsung",
+    organiser: "Samsung",
+    description: "Witness power like never before.",
+    isLatest: true,
+    videoId: "CmS5rlX9cDA",
+    videoUrl: "https://www.youtube.com/watch?v=CmS5rlX9cDA",
+  },
+  {
+    title: "Galaxy Unpacked: Official Replay - 2019",
+    organiser: "Samsung",
+    description:
+      "Watch the unveiling of the new Galaxy devices at Unpacked 2019.",
+    isLatest: false,
+    videoId: "USDvd7fhbd0",
+    videoUrl: "https://www.youtube.com/watch?v=USDvd7fhbd0",
+  },
+
+  {
+    title: "Samsung Galaxy Note 8 Presentation",
+    organiser: "Samsung",
+    description:
+      "Samsung Galaxy Note 8 presentation in New York this August-2017.",
+    isLatest: false,
+    videoId: "0OSx0oQZzz4",
+    videoUrl: "https://www.youtube.com/watch?v=0OSx0oQZzz4",
+  },
 
   //MICROSOFT
 
@@ -77,6 +151,16 @@ export const AllVideos = [
   },
 
   {
+    title: "Vision Keynote | Microsoft Ignite 2019",
+    organiser: "Microsoft",
+    description:
+      "Microsoft Ignite is for anyone who is motivated to be on the frontier of innovation and tech. Learn more about the event.",
+    isLatest: false,
+    videoId: "jnUiJi4hts4",
+    videoUrl: "https://www.youtube.com/watch?v=jnUiJi4hts4",
+  },
+
+  {
     title: "#MicrosoftEvent​ Live",
     organiser: "Microsoft",
     description:
@@ -84,6 +168,26 @@ export const AllVideos = [
     isLatest: false,
     videoId: "dmaioTs0NH8",
     videoUrl: "https://www.youtube.com/watch?v=dmaioTs0NH8",
+  },
+
+  {
+    title: "Microsoft Build 2018 | Vision Keynote",
+    organiser: "Microsoft",
+    description:
+      "CEO Satya Nadella takes the stage at Microsoft Build, our annual developer conference.",
+    isLatest: false,
+    videoId: "rd0Rd8w3FZ0",
+    videoUrl: "https://www.youtube.com/watch?v=rd0Rd8w3FZ0",
+  },
+
+  {
+    title: "Microsoft Build 2017",
+    organiser: "Microsoft",
+    description:
+      "CEO Satya Nadella takes the stage at Microsoft Build, our annual developer conference.",
+    isLatest: false,
+    videoId: "nRkKoWx-_38",
+    videoUrl: "https://www.youtube.com/watch?v=nRkKoWx-_38",
   },
 
   //   CES
@@ -104,5 +208,32 @@ export const AllVideos = [
     isLatest: false,
     videoId: "mHreov2zl1U",
     videoUrl: "https://www.youtube.com/watch?v=mHreov2zl1U",
+  },
+
+  {
+    title: "The Best of CES 2018",
+    organiser: "CES",
+    description: "The Best of CES 2018: Only the cream of the crop",
+    isLatest: false,
+    videoId: "8UCOkXe6fyY",
+    videoUrl: "https://www.youtube.com/watch?v=8UCOkXe6fyY",
+  },
+
+  {
+    title: "The Best of CES 2017",
+    organiser: "CES",
+    description: "The Best of CES 2017: Only the cream of the crop",
+    isLatest: false,
+    videoId: "kjXizY4sJbY",
+    videoUrl: "https://www.youtube.com/watch?v=kjXizY4sJbY",
+  },
+
+  {
+    title: "The Best of CES 2015",
+    organiser: "CES",
+    description: "The Best of CES 2015: Only the cream of the crop",
+    isLatest: false,
+    videoId: "jTGb3ubedgU",
+    videoUrl: "https://www.youtube.com/watch?v=jTGb3ubedgU",
   },
 ];
