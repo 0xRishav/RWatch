ff4fa18fb4f7fcb1ebf75f93c3e79c1595692824
Rishav Bharti <rshvbharti@gmail.com>
2021-04-23 22:56:16 +0530

    Added search page

---
+++ src/pages/search-page/SearchPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useLocation } from "react-router";
+import { NormalVideoCard } from "../../components";
+import "./SearchPage.css";
+
+function SearchPage() {
+  const { state } = useLocation();
+  const { searchInput, filteredVideos } = state;
+  return (
+    <div className="searchpage">
+      <h1>Search results for {searchInput}</h1>
+      <div className="allvidespage__videosWrapper">
+        {filteredVideos.length === 0 && <h3>Nothing to show</h3>}
+        {filteredVideos.length > 0 &&
+          filteredVideos.map((video) => (
+            <NormalVideoCard {...video} isInGrid />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default SearchPage;
