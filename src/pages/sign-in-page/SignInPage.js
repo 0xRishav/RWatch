38e790ae051e09276c65c2e4b3b7ed32731d5d65
Rishav Bharti <rshvbharti@gmail.com>
2021-04-23 16:46:35 +0530

    Added signin page

--- src/pages/sign-in-page/SignInPage.js
+++ src/pages/sign-in-page/SignInPage.js
@@ -3,42 +3,50 @@ import { Link, useHistory, useLocation } from "react-router-dom";
 import "./SignInPage.css";
 import { BsArrowUpRight } from "react-icons/bs";
 import { authContext } from "../../context/authContext";
+import { Loader } from "../../components";
 
 function SignInPage() {
-  //   const { loginWithCredentials } = useContext(authContext);
   const { loginWithCredentials } = useContext(authContext);
   const { state } = useLocation();
+  const [isSignInProgress, setIsSignInProgress] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   let history = useHistory();
 
   async function signinHandler() {
+    setIsSignInProgress(true);
     const response = await loginWithCredentials(email, password);
     if (response.success) {
       history.push(state?.from ? state.from : "/");
     }
+    setIsSignInProgress(false);
   }
 
   console.log({ state });
 
   return (
     <div className="signin">
-      <h2>Sign in to RStore</h2>
+      <h1>Sign in to RWatch</h1>
       <div className="signin__inputContainer">
         <input
           type="email"
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
+          className="signin__input"
         />
         <input
           type="password"
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
+          className="signin__input"
         />
       </div>
-      <button className="blue-btn--primary" onClick={signinHandler}>
-        Sign in
-      </button>
+      {isSignInProgress && <Loader />}
+      {!isSignInProgress && (
+        <button className="signin__btn" onClick={signinHandler}>
+          Sign in
+        </button>
+      )}
       <div>
         Do not have an RStore account?{" "}
         <Link to="/signup">
