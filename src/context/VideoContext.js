8d387f09393c66c6db3722d68b7c0f177dc25f92
Rishav Bharti <rshvbharti@gmail.com>
2021-04-23 22:57:38 +0530

    feat: Added reducer for state management

--- src/context/VideoContext.js
+++ src/context/VideoContext.js
@@ -1,10 +1,29 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 import { AllVideos } from "../data/AllVideos";
 
 export const videoContext = createContext();
 
 export const VideoContextProvider = ({ children }) => {
+  const initialState = {
+    playlists: {
+      likedVideos: [],
+      watchLater: [],
+    },
+  };
+
+  const videoReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD_PLAYLIST":
+        return {
+          ...state,
+          playlists: { ...state.playlists, [action.payload]: [] },
+        };
+    }
+  };
+  const [state, dispatch] = useReducer(videoReducer, initialState);
   return (
-    <videoContext.Provider value={AllVideos}>{children}</videoContext.Provider>
+    <videoContext.Provider value={{ AllVideos, ...state, dispatch }}>
+      {children}
+    </videoContext.Provider>
   );
 };
