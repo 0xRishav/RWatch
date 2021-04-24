61aef800ceae4e9d0a0907d1675e372da1e66829
Rishav Bharti <rshvbharti@gmail.com>
2021-04-24 23:48:02 +0530

    Added playlist modal for homepage video

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,18 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import { AllVideoListing } from "../../containers";
 import { videoContext } from "../../context/VideoContext";
 import "./Homepage.css";
+import { PlaylistModal } from "../../components";
 
 function Homepage() {
-  const AllVideos = useContext(videoContext);
+  const { AllVideos } = useContext(videoContext);
 
   const truncate = (string, n) => {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   };
 
-  const { title, description, videoId } = AllVideos.find(
+  const video = AllVideos.find(
     (video) => video.title === "High Speed - October 13"
   );
+
+  const { title, description, videoId } = video;
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    console.log("CLICKED");
+    setIsOpen(true);
+  }
+
   return (
     <div className="homepage">
       <header
@@ -24,13 +36,27 @@ function Homepage() {
         <div className="homepage__bannerContent">
           <h1 className="homepage__bannerTitle">{title}</h1>
           <div className="homepage__bannerButtons">
-            <button className="homepage__bannerButton">Play</button>
-            <button className="homepage__bannerButton">My List</button>
+            <Link to={`/videos/${videoId}`}>
+              <button className="homepage__bannerButton">Play</button>
+            </Link>
+            <button className="homepage__bannerButton" onClick={openModal}>
+              Add To Playlist
+            </button>
           </div>
-          <h3 className="homepage__bannerDiscription">{truncate(description, 150)}</h3>
+          <h3 className="homepage__bannerDiscription">
+            {truncate(description, 150)}
+          </h3>
         </div>
         <div className="fade--bottom" />
       </header>
+
+      <PlaylistModal
+        modalIsOpen={modalIsOpen}
+        setIsOpen={setIsOpen}
+        video={video}
+        openModal={openModal}
+      />
+
       <AllVideoListing />
     </div>
   );
