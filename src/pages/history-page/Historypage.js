89178215b57224704d24b94155515efb5e510f6c
Rishav Bharti <rshvbharti@gmail.com>
2021-04-25 02:51:22 +0530

    Completed history page

--- src/pages/history-page/Historypage.js
+++ src/pages/history-page/Historypage.js
@@ -1,13 +1,30 @@
 import { useContext } from "react";
+import { NormalVideoList } from "../../containers";
 import { videoContext } from "../../context/VideoContext";
 import "./Historypage.css";
 
 const Historypage = () => {
-  const { history, AllVideos } = useContext(videoContext);
-  const historyVideos = AllVideos.map(video=>{return})
+  const { history, dispatch } = useContext(videoContext);
+
   return (
     <div className="historypage">
-      <h1>History Page</h1>
+      {
+        <div className="">
+          {history.length !== 0 && History}
+          {history.length !== 0 && (
+            <NormalVideoList videos={history} dontShowTitle />
+          )}
+        </div>
+      }
+      {history.length === 0 && <h1>Nothing to show</h1>}
+      {history.length !== 0 && (
+        <button
+          className="historypage__clearHistoryBtn"
+          onClick={() => dispatch({ type: "CLEAR_HISTORY" })}
+        >
+          Clear History
+        </button>
+      )}
     </div>
   );
 };
