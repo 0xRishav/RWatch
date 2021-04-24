04e831701edabb347bf97e12803307445696c8ac
Rishav Bharti <rshvbharti@gmail.com>
2021-04-25 02:51:46 +0530

    Completed playlist page

--- src/pages/playlist-page/PlaylistPage.js
+++ src/pages/playlist-page/PlaylistPage.js
@@ -1,9 +1,46 @@
+import { useContext, useEffect, useState } from "react";
+import { videoContext } from "../../context/VideoContext";
+import { NormalVideoList } from "../../containers";
 import "./PlaylistPage.css";
 
 const PlaylistPage = () => {
+  const { playlists, likedVideos } = useContext(videoContext);
+  const [isNothing, setIsNothing] = useState(false);
+
+  useEffect(() => {
+    playlists.map((playlist) =>
+      playlist.playlist.length === 0 ? setIsNothing(true) : setIsNothing(false)
+    );
+    likedVideos.length === 0 ? setIsNothing(true) : setIsNothing(false);
+  }, [playlists]);
+
   return (
-    <div>
-      <h1>Playlist</h1>
+    <div className="playlistpage">
+      {playlists.map((playlist) => (
+        <div className="">
+          {playlist.playlist.length !== 0 && <h1>{playlist.name}</h1>}
+          {playlist.playlist.length !== 0 && (
+            <NormalVideoList
+              videos={playlist.playlist}
+              dontShowTitle
+              inGrid={false}
+            />
+          )}
+        </div>
+      ))}
+      {
+        <div className="">
+          {likedVideos.length !== 0 && <h1>Liked Videos</h1>}
+          {likedVideos.length !== 0 && (
+            <NormalVideoList
+              videos={likedVideos}
+              dontShowTitle
+              inGrid={false}
+            />
+          )}
+        </div>
+      }
+      {isNothing && <h1>Nothing to show</h1>}
     </div>
   );
 };
